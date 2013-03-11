bbd49ddb3341d4b8f20fed628ce19faf2440644d
Charles SANQUER <charles.sanquer@spyrit.net>
2013-03-11 17:06:04 +0100

    fix missing console methods on IE8

--- fauxconsole.js
+++ fauxconsole.js
@@ -5,51 +5,7 @@
 (function (window) {
     "use strict"; // mostly for me and my IDE :)
 
-    var consoleDiv, consoleContent;
-
-    if (typeof window.console === 'undefined') {
-        consoleDiv = createElement('div', { className: 'fauxconsole' });
-
-        consoleDiv.appendChild(createElement('a', { href: 'javascript:console.hide()' }, 'hide'));
-        consoleDiv.appendChild(createElement('a', { href: 'javascript:console.show()' }, 'show'));
-        consoleDiv.appendChild(createElement('a', { href: 'javascript:console.clear()' }, 'clear'));
-
-        consoleContent = createElement('pre');
-        consoleDiv.appendChild(consoleContent);
-
-        (document.body || document.documentElement).appendChild(consoleDiv);
-        hide();
-
-        window.console = {
-            debug: log,
-            info: log,
-            log: log,
-            warn: log,
-            error: log,
-            assert: log,
-            show: show,
-            hide: hide,
-            clear: clear
-        };
-    }
-
     // --- some tools ---
-
-    function log () {
-        for (var i = 0, l = arguments.length; i < l; ++i) {
-            consoleContent.innerHTML += '<br/><br/>' + arguments[i];
-        }
-        show();
-    }
-
-    function show () { consoleDiv.style.display = 'block'; }
-
-    function hide () { consoleDiv.style.display = 'none'; }
-
-    function clear () {
-        consoleContent.innerHTML = '';
-    }
-
     function createElement(tagName, attributes, innerHTML) {
         var element = document.createElement(tagName);
         for (var attr in attributes) {
@@ -63,5 +19,84 @@
         return element;
     }
 
+    var consoleDiv, consoleContent;
+    
+    //check existence of console
+    if (typeof window.console === 'undefined' || typeof window.console.log === 'undefined') {
+        consoleDiv = createElement('div', { className: 'fauxconsole' });
 
+        consoleDiv.appendChild(createElement('a', { href: 'javascript:console.hide()' }, 'hide'));
+        consoleDiv.appendChild(createElement('a', { href: 'javascript:console.show()' }, 'show'));
+        consoleDiv.appendChild(createElement('a', { href: 'javascript:console.clear()' }, 'clear'));
+
+        consoleContent = createElement('pre');
+        consoleDiv.appendChild(consoleContent);
+
+        (document.body || document.documentElement).appendChild(consoleDiv);
+        hide();
+
+        window.console = {
+            show: function show() { 
+                consoleDiv.style.display = 'block'; 
+            },
+            hide: function hide() { 
+                consoleDiv.style.display = 'none'; 
+            },
+            clear: function clear () {
+                consoleContent.innerHTML = '';
+            },
+            log: function log() {
+                for (var i = 0, l = arguments.length; i < l; ++i) {
+                    consoleContent.innerHTML += '<br/><br/>' + arguments[i];
+                }
+                show();
+            }
+            
+        };
+    }
+    
+    console.log(window.console.debug);
+    if (typeof window.console.debug === 'undefined') {
+        window.console.debug = window.console.log;
+    }
+    
+    console.log(window.console.info);
+    if (typeof window.console.info === 'undefined') {
+        window.console.info = window.console.log;
+    }
+    
+    console.log(window.console.warn);
+    if (typeof window.console.warn === 'undefined') {
+        window.console.warn = window.console.log;
+    }
+    
+    console.log(window.console.error);
+    if (typeof window.console.error === 'undefined') {
+        window.console.error = window.console.log;
+    }
+    
+    console.log(window.console.assert);
+    if (typeof window.console.assert === 'undefined') {
+        window.console.assert = function assert() {
+            if (!arguments[0]) {
+                if (arguments[1]) {
+                    window.console.log(arguments[1]);
+                } else {
+                    window.console.log('Assertion failed');
+                }
+            }
+        };
+    }
+    
+    if (typeof window.console.show === 'undefined') {
+        window.console.show = function () {};
+    }
+    
+    if (typeof window.console.hide === 'undefined') {
+        window.console.hide = function () {};
+    }
+    
+    if (typeof window.console.clear === 'undefined') {
+        window.console.clear = function () {};
+    }
 }(window, window.console, 'fauxconsole'));
